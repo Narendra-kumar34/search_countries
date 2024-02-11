c68c098a0110563ed22959fb172c7877cd2824de
Narendra <narendrach34@gmail.com>
2024-02-11 20:39:57 +0530

    implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [debounceTimer, setDebounceTimer] = useState(0);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      let response = await axios.get(`https://restcountries.com/v3.1/all`);
+      setCountries(response.data);
+      setFilteredCountries(response.data);
+    } catch (err) {
+      console.log("Error fetching data: ", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (debounceTimer !== 0) {
+      clearTimeout(debounceTimer);
+    }
+    let timer = setTimeout(() => {
+      if (search === "") {
+        fetchData();
+      } else {
+        const filtered = countries.filter((country) =>
+          country.name.common.toLowerCase().includes(search.toLowerCase())
+        );
+        setFilteredCountries(filtered);
+      }
+    }, 500);
+    setDebounceTimer(timer);
+  }, [search]);
+
+  const card = {
+    width: "200px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    margin: "10px",
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+  };
+
+  const container = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div style={container}>
+        {filteredCountries.map((country) => (
+          <div style={card} key={country.name.common} className="countryCard">
+            <img
+              src={country.flags?.png}
+              alt={country.name.common}
+              style={imageStyle}
+            />
+            <h1>{country.name.common}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
